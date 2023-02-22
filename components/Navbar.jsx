e2fb9665df5f6b669a589d46c72596a2bcda888e
WasathTheekshana <wasath.vt@gmail.com>
2023-02-22 21:31:38 +0530

    dev: pages creates

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+function Navbar() {
+    return (
+        <nav className="text-2xl font-medium ">
+            <ul className="flex gap-12">
+                <Link href={"/"}> <li>Home</li> </Link>
+                <Link href={"/Contact"}> <li>Contact</li> </Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import React from "react";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="py-24 px-12 lg:px-48">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
